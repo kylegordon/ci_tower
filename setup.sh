6bb2f85523dc62e8a7ab672df167a60078b894c7
Kyle Gordon <kyle.gordon@linn.co.uk>
2015-05-11 12:09:22 +0100

    Unify setup. Also lets it be run from anywhere on the FS (ie, be called from rc.local)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-./export_gpio.sh
-./direct_gpio.sh
-./ripple.sh
+# Export the GPIO pins
+echo 11 > /sys/class/gpio/export
+echo 12 > /sys/class/gpio/export
+echo 13 > /sys/class/gpio/export
+
+# Set their direction
+echo out > /sys/class/gpio/gpio11/direction
+echo out > /sys/class/gpio/gpio12/direction
+echo out > /sys/class/gpio/gpio13/direction
